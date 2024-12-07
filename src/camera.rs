5b9aa2ba6337d2893922fac0f8439d2bc4113d9d
RasmusBruhn <rasmusbruhnnielsen@gmail.com>
2024-12-07 20:14:13 +0100

    Moved the settings out of the camera into their own struct and removed the functions to manually set the camera input

--- src/camera.rs
+++ src/camera.rs
@@ -6,7 +6,10 @@ use winit::{
 use super::{constants::SQRT_3, types};
 
 /// Describes a how the camera is moving
-pub struct Camera {
+#[derive(Clone, Copy, Debug)]
+pub struct HexCamera {
+    /// All the settings
+    settings: HexCameraSettings,
     /// The movement keys: d, e, w, a, z, x
     active_move: [bool; 6],
     /// The zoom keys: s, q
@@ -15,14 +18,6 @@ pub struct Camera {
     active_rotate: [bool; 2],
     /// True if any button is pressed and the camera needs to be updated
     active: bool,
-    /// The speed of movement
-    speed_move: f64,
-    /// The speed of zooming
-    speed_zoom: f64,
-    /// The speed of rotation
-    speed_rotate: f64,
-    /// The framerate of the program, this is how many times a second the transform should be updated
-    framerate: f64,
     /// The current transform
     transform: types::Transform2D,
     /// The transform to make the aspect ratio correct
@@ -31,7 +26,7 @@ pub struct Camera {
     transform_update: types::Transform2D,
 }
 
-impl Camera {
+impl HexCamera {
     /// Creates a new camera
     ///
     /// # Parameters
@@ -42,10 +37,7 @@ impl Camera {
     ///
     /// size: The current size of the window
     pub fn new(
-        speed_move: f64,
-        speed_zoom: f64,
-        speed_rotate: f64,
-        framerate: f64,
+        settings: &HexCameraSettings,
         transform: &types::Transform2D,
         size: &winit::dpi::PhysicalSize<u32>,
     ) -> Self {
@@ -54,71 +46,21 @@ impl Camera {
             active_zoom: [false; 2],
             active_rotate: [false; 2],
             active: false,
-            speed_move,
-            speed_zoom,
-            speed_rotate,
-            framerate,
+            settings: *settings,
             transform: *transform,
             transform_aspect: Self::size_to_aspect(size),
             transform_update: types::Transform2D::identity(),
         }
     }
 
-    /// Set one of the movement keys, id 0-5 for d, e, w, a, z, x
-    ///
-    /// # Parameters
-    ///
-    /// id: The id of the key to set
-    ///
-    /// active: True if it is pressed down
-    pub fn set_key_move(&mut self, id: usize, active: bool) {
-        self.active_move[id] = active;
-        self.reload_transform();
+    /// Retrieves a reference to the settings
+    pub fn get_settings(&self) -> &HexCameraSettings {
+        return &self.settings;
     }
 
-    /// Set one of the zoom keys, id 0-1 for s, q
-    ///
-    /// # Parameters
-    ///
-    /// id: The id of the key to set
-    ///
-    /// active: True if it is pressed down
-    pub fn set_key_zoom(&mut self, id: usize, active: bool) {
-        self.active_zoom[id] = active;
-        self.reload_transform();
-    }
-
-    /// Set one of the rotate keys, id 0-1 for r, c
-    ///
-    /// # Parameters
-    ///
-    /// id: The id of the key to set
-    ///
-    /// active: True if it is pressed down
-    pub fn set_key_rotate(&mut self, id: usize, active: bool) {
-        self.active_rotate[id] = active;
-        self.reload_transform();
-    }
-
-    /// Sets all the keys
-    ///
-    /// # Parameters
-    ///
-    /// active_move: Tells if each move key is active or nor
-    ///
-    /// active_zoom: Tells if each zoom key is active or not
-    ///
-    /// active_rotate: Tells if each rotate key is active or not
-    pub fn set_keys(
-        &mut self,
-        active_move: &[bool; 6],
-        active_zoom: &[bool; 2],
-        active_rotate: &[bool; 2],
-    ) {
-        self.active_move = *active_move;
-        self.active_zoom = *active_zoom;
-        self.active_rotate = *active_rotate;
-        self.reload_transform();
+    /// Constructs a mutator for the settings
+    pub fn get_settings_mut(&mut self) -> HexCameraSettingsMut {
+        return HexCameraSettingsMut { camera: self };
     }
 
     /// Attempts to use a key press from a key event, if the key press is used,
@@ -133,115 +75,34 @@ impl Camera {
             ElementState::Released => false,
         };
 
-        return match event.physical_key {
-            PhysicalKey::Unidentified(_) => false,
+        match event.physical_key {
+            PhysicalKey::Unidentified(_) => return false,
             PhysicalKey::Code(code) => match code {
-                KeyCode::KeyD => {
-                    self.set_key_move(0, active);
-                    true
-                }
-                KeyCode::KeyE => {
-                    self.set_key_move(1, active);
-                    true
-                }
-                KeyCode::KeyW => {
-                    self.set_key_move(2, active);
-                    true
-                }
-                KeyCode::KeyA => {
-                    self.set_key_move(3, active);
-                    true
-                }
-                KeyCode::KeyZ => {
-                    self.set_key_move(4, active);
-                    true
-                }
-                KeyCode::KeyX => {
-                    self.set_key_move(5, active);
-                    true
-                }
-                KeyCode::KeyS => {
-                    self.set_key_zoom(0, active);
-                    true
-                }
-                KeyCode::KeyQ => {
-                    self.set_key_zoom(1, active);
-                    true
-                }
-                KeyCode::KeyR => {
-                    self.set_key_rotate(0, active);
-                    true
-                }
-                KeyCode::KeyC => {
-                    self.set_key_rotate(1, active);
-                    true
-                }
-                _ => false,
+                KeyCode::KeyD => self.active_move[0] = active,
+                KeyCode::KeyE => self.active_move[1] = active,
+                KeyCode::KeyW => self.active_move[2] = active,
+                KeyCode::KeyA => self.active_move[3] = active,
+                KeyCode::KeyZ => self.active_move[4] = active,
+                KeyCode::KeyX => self.active_move[5] = active,
+                KeyCode::KeyS => self.active_zoom[0] = active,
+                KeyCode::KeyQ => self.active_zoom[1] = active,
+                KeyCode::KeyR => self.active_rotate[0] = active,
+                KeyCode::KeyC => self.active_rotate[1] = active,
+                _ => return false,
             },
         };
+
+        // Reload the update transform
+        self.reload_transform();
+
+        return true;
     }
 
     /// Reset all of the input such that all of it is turned off
-    pub fn reset_updates(&mut self) {
+    pub fn reset_keys(&mut self) {
         self.active_move.iter_mut().for_each(|val| *val = false);
         self.active_zoom.iter_mut().for_each(|val| *val = false);
         self.active_rotate.iter_mut().for_each(|val| *val = false);
-        self.reload_transform();
-    }
-
-    /// Sets the movement speed
-    ///
-    /// # Parameters
-    ///
-    /// speed: The new movement speed
-    pub fn set_speed_move(&mut self, speed: f64) {
-        self.speed_move = speed;
-        self.reload_transform();
-    }
-
-    /// Sets the zoom speed
-    ///
-    /// # Parameters
-    ///
-    /// speed: The new zoom speed
-    pub fn set_speed_zoom(&mut self, speed: f64) {
-        self.speed_zoom = speed;
-        self.reload_transform();
-    }
-
-    /// Sets the rotation speed
-    ///
-    /// # Parameters
-    ///
-    /// speed: The new rotation speed
-    pub fn set_speed_rotate(&mut self, speed: f64) {
-        self.speed_rotate = speed;
-        self.reload_transform();
-    }
-
-    /// Sets all speeds
-    ///
-    /// # Parameters
-    ///
-    /// speed_move: The new movement speed
-    ///
-    /// speed_zoom: The new zoom speed
-    ///
-    /// speed_rotate: The new rotation speed
-    pub fn set_speeds(&mut self, speed_move: f64, speed_zoom: f64, speed_rotate: f64) {
-        self.speed_move = speed_move;
-        self.speed_zoom = speed_zoom;
-        self.speed_rotate = speed_rotate;
-        self.reload_transform();
-    }
-
-    /// Sets the framerate for if it changes
-    ///
-    /// # Parameters
-    ///
-    /// framerate: The new framerate, this is how many times a second the transform should be updated
-    pub fn set_framerate(&mut self, framerate: f64) {
-        self.framerate = framerate;
         self.reload_transform();
     }
 
@@ -293,7 +154,7 @@ impl Camera {
         }
 
         // Calculate the movement velocity
-        let move_speed = self.speed_move / self.framerate;
+        let move_speed = self.settings.speed_move / self.settings.framerate;
         let mut move_dir = self
             .active_move
             .iter()
@@ -305,7 +166,7 @@ impl Camera {
         }
 
         // Calculate the zoom velocity
-        let zoom_val = 1.0 + self.speed_zoom / self.framerate;
+        let zoom_val = 1.0 + self.settings.speed_zoom / self.settings.framerate;
         let key_zoom = [zoom_val, 1.0 / zoom_val];
         let zoom_dir = self
             .active_zoom
@@ -315,7 +176,7 @@ impl Camera {
             .fold(1.0, |prev, next| prev * next);
 
         // Calculate the rotation velocity
-        let rotate_val = self.speed_rotate / self.framerate;
+        let rotate_val = self.settings.speed_rotate / self.settings.framerate;
         let key_rotate = [-rotate_val, rotate_val];
         let rotate_dir = self
             .active_rotate
@@ -365,3 +226,94 @@ const KEY_DIRECTION_HEX: [types::Point; 6] = [
         y: -0.5 * SQRT_3,
     },
 ];
+
+/// A mutator for the hex camera settings, the transform for the camera is
+/// reloaded once the mutator is dropped
+#[derive(Debug)]
+pub struct HexCameraSettingsMut<'cam> {
+    camera: &'cam mut HexCamera,
+}
+
+impl<'map> HexCameraSettingsMut<'map> {
+    /// Retrieves a reference to the settings
+    pub fn get(&self) -> &HexCameraSettings {
+        return &self.camera.settings;
+    }
+
+    /// Retrieves a mutable reference to the settings
+    pub fn get_mut(&mut self) -> &mut HexCameraSettings {
+        return &mut self.camera.settings;
+    }
+}
+
+impl<'map> Drop for HexCameraSettingsMut<'map> {
+    fn drop(&mut self) {
+        // Force update the camera transform
+        self.camera.reload_transform();
+    }
+}
+
+/// All settings for a camera
+#[derive(Clone, Copy, Debug)]
+pub struct HexCameraSettings {
+    /// The speed of movement
+    pub speed_move: f64,
+    /// The speed of zooming
+    pub speed_zoom: f64,
+    /// The speed of rotation
+    pub speed_rotate: f64,
+    /// The framerate of the program, this is how many times a second the transform should be updated
+    pub framerate: f64,
+}
+
+impl HexCameraSettings {
+    /// Creates camera settings with default values
+    pub fn default() -> Self {
+        return Self {
+            speed_move: 4.0,
+            speed_zoom: 1.2,
+            speed_rotate: 1.0,
+            framerate: 60.0,
+        };
+    }
+
+    /// Changes the movement speed and returns the updated object
+    ///
+    /// # Parameters
+    ///
+    /// speed: The new movement speed
+    pub fn with_speed_move(mut self, speed: f64) -> Self {
+        self.speed_move = speed;
+        return self;
+    }
+
+    /// Changes the zoom speed and returns the updated object
+    ///
+    /// # Parameters
+    ///
+    /// speed: The new zoom speed
+    pub fn with_speed_zoom(mut self, speed: f64) -> Self {
+        self.speed_zoom = speed;
+        return self;
+    }
+
+    /// Changes the rotation speed and returns the updated object
+    ///
+    /// # Parameters
+    ///
+    /// speed: The new rotation speed
+    pub fn with_speed_rotate(mut self, speed: f64) -> Self {
+        self.speed_rotate = speed;
+        return self;
+    }
+
+    /// Changes the framerate and returns the updated object
+    ///
+    /// # Parameters
+    ///
+    /// framerate: The new framerate
+    pub fn with_framerate(mut self, framerate: f64) -> Self {
+        self.framerate = framerate;
+        return self;
+    }
+}
